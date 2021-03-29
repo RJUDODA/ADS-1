cb3a41bb37ab28e2930b39ba563fde43115de2a7
RJUDODA <78902484+RJUDODA@users.noreply.github.com>
2021-03-29 21:31:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,3 +1,5 @@
+// Copyright 2021 NNTU-CS
+
 int cbinsearch(int *arr, int size, int value) {
 int i = 0, j = size - 1, c = 0;
 while (i < j) {
@@ -7,14 +9,16 @@ i = m + 1;
 else
 j = m;
 }
+
 if (arr[i] == value) {
 while (arr[j] == value) {
 c++;
 i++;
 }
 }
+
 if (c)
 return c;
 else
-return 0;
+return 0; // если ничего не найдено
 }
