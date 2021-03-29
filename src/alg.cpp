0bb07cb5ba4ce2eb3ccbb45ed3d6766c1ff6b5ed
RJUDODA <78902484+RJUDODA@users.noreply.github.com>
2021-03-29 21:13:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,23 +1,21 @@
 int cbinsearch(int *arr, int size, int value) {
 int i = 0, j = size - 1, c = 0;
-while (i < j)
-{
+while (i < j) {
 int m = (i + j) / 2;
 if (arr[m] < value)
 i = m + 1;
 else
 j = m;
 }
-if (arr[i] == value) 
-  {
-while (arr[j] == value) 
-    {
+
+if (arr[i] == value) {
+while (arr[j] == value) {
 c++;
 i++;
-  }
-    }
+}
+}
+
 if (c)
 return c;
 else
 return 0;
-}
